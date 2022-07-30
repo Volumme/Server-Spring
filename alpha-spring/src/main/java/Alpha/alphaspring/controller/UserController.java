cc393a78b305a2473b2e156a21a0828d955b66f9
jykim-rust <alexj99@naver.com>
2022-07-30 15:55:48 +0900

    [JYK] Fix Error in server

--- alpha-spring/src/main/java/Alpha/alphaspring/controller/UserController.java
+++ alpha-spring/src/main/java/Alpha/alphaspring/controller/UserController.java
@@ -4,11 +4,6 @@ import Alpha.alphaspring.domain.User;
 import Alpha.alphaspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;

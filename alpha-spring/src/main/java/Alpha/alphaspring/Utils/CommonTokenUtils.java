7e5dee218437cb6fad09aec568cf8b77562470b3
김형준 <ninini6630@gmail.com>
2022-08-14 00:47:09 +0900

    [KHJ] Convert CommonTokenUtils class to Spring bean component

--- alpha-spring/src/main/java/Alpha/alphaspring/Utils/CommonTokenUtils.java
+++ alpha-spring/src/main/java/Alpha/alphaspring/Utils/CommonTokenUtils.java
@@ -2,7 +2,9 @@ package Alpha.alphaspring.Utils;
 
 import com.auth0.jwk.JwkException;
 import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommonTokenUtils extends AbstractTokenUtils{
     @Override
     public boolean validate(String token) throws ParseException, JwkException {

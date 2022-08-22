54324bc702e7309b52789ee02839af59b9c99d98
jykim-rust <alexj99@naver.com>
2022-08-22 20:17:37 +0900

    [JYK] Change method name, but need feedback

--- alpha-spring/src/main/java/Alpha/alphaspring/repository/RoutineRepository.java
+++ alpha-spring/src/main/java/Alpha/alphaspring/repository/RoutineRepository.java
@@ -2,6 +2,7 @@ package Alpha.alphaspring.repository;
 
 import Alpha.alphaspring.domain.Routine;
 import Alpha.alphaspring.domain.Routine;
+import Alpha.alphaspring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,7 @@ public interface RoutineRepository extends JpaRepository<Routine, Long> {
     Routine save(Routine routine);
     Optional<Routine> findById(String id);
     Optional<Routine> findByName(String name);
+    Optional<Routine> findByNameAndUser(String name, User user);
     List<Routine> findAll();
-    Optional<Routine> findByUser_Id(Long userId);
+    Optional<Routine> findByUserId(Long userId);
 }

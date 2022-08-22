959795dbcf5977614ff96c47b065280cc8abe8ff
jykim-rust <alexj99@naver.com>
2022-08-22 20:24:24 +0900

    [JYK] Add builder, constructor

--- alpha-spring/src/main/java/Alpha/alphaspring/domain/SubRoutine.java
+++ alpha-spring/src/main/java/Alpha/alphaspring/domain/SubRoutine.java
@@ -1,7 +1,6 @@
 package Alpha.alphaspring.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -10,6 +9,9 @@ import java.util.List;
 @Entity
 @Setter
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubRoutine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

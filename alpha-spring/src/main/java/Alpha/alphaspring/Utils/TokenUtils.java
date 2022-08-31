90c864d5ef5f2a67beb733c8aaf6c23c8827e53c
jykim-rust <alexj99@naver.com>
2022-08-31 18:10:25 +0900

    [JYK] Add supports method in TokenUtils

--- alpha-spring/src/main/java/Alpha/alphaspring/Utils/TokenUtils.java
+++ alpha-spring/src/main/java/Alpha/alphaspring/Utils/TokenUtils.java
@@ -4,6 +4,8 @@ import com.auth0.jwk.JwkException;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Base64;
 
 public interface TokenUtils {
@@ -14,4 +16,5 @@ public interface TokenUtils {
     String getClaim(String token, String key, String scope) throws ParseException;
     boolean validate(String token) throws ParseException, JwkException;
     OauthInfo getOauthInfo(String token) throws ParseException;
+    boolean supports(String token);
 }

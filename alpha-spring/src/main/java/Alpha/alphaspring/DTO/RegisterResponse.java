e244d91c0a80b3229882560d2feac319111813d4
jykim-rust <alexj99@naver.com>
2022-08-31 18:18:51 +0900

    [JYK] Impl SNSFilter to handle SNS login

---
+++ alpha-spring/src/main/java/Alpha/alphaspring/DTO/RegisterResponse.java
@@ -0,0 +1,9 @@
+package Alpha.alphaspring.DTO;
+
+public class RegisterResponse {
+    private final String response;
+
+    public RegisterResponse() {
+        this.response = "success!!";
+    }
+}

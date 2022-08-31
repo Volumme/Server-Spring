039d0bf0ca006f1de3ac0f3de897d043dcde592c
jykim-rust <alexj99@naver.com>
2022-08-31 18:03:48 +0900

    [JYK] Use JwtTokenUtils

--- alpha-spring/src/main/java/Alpha/alphaspring/filter/JwtAuthenticationProvider.java
+++ alpha-spring/src/main/java/Alpha/alphaspring/filter/JwtAuthenticationProvider.java
@@ -1,11 +1,8 @@
 package Alpha.alphaspring.filter;
 
 import Alpha.alphaspring.DTO.UserDetails;
-import Alpha.alphaspring.Utils.KakaoTokenUtils;
-import Alpha.alphaspring.Utils.OauthInfo;
-import Alpha.alphaspring.Utils.TokenUtils;
-import Alpha.alphaspring.service.UserDetailsServiceImpl;
-import Alpha.alphaspring.service.UserServiceImpl;
+import Alpha.alphaspring.Utils.*;
+import Alpha.alphaspring.service.UserDetailsService;
 import com.auth0.jwk.JwkException;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +11,28 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 @Component
 public class JwtAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
-    UserDetailsServiceImpl userDetailsService;
+    UserDetailsService userDetailsService;
+    @Autowired
+    JwtTokenUtils jwtTokenUtils;
 
     @Autowired
-    KakaoTokenUtils tokenUtils;
+    KakaoTokenUtils kakaoTokenUtils;
+    @Autowired
+    GoogleTokenUtils googleTokenUtils;
+//    @Autowired
+//    NaverTokenUtils naverTokenUtils;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -35,14 +40,13 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
         String jwtToken = beforeToken.getCredential();
 
         try {
-            if(!tokenUtils.validate(jwtToken)){
-                throw new BadCredentialsException("token not valid");
+            if (!jwtTokenUtils.validate(jwtToken)){
+                throw  new BadCredentialsException("token not valid");
             }
-            OauthInfo oauthInfo = tokenUtils.getOauthInfo(jwtToken);
+            OauthInfo oauthInfo = jwtTokenUtils.getOauthInfo(jwtToken);
             String userId = oauthInfo.getUserId();
             String provider = oauthInfo.getProvider();
             UserDetails user = userDetailsService.findUser(userId, provider);
-
             return new JwtAuthenticationToken(user, user.getAuthorities());
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
@@ -54,3 +58,6 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
         return JwtAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
+
+
+

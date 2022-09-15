257cd8a9fe7f6e46dfa624429052d16d0d6d331c
jykim-rust <alexj99@naver.com>
2022-09-15 20:02:02 +0900

    [JYK] User name id provided by sns

--- alpha-spring/src/main/java/Alpha/alphaspring/domain/User.java
+++ alpha-spring/src/main/java/Alpha/alphaspring/domain/User.java
@@ -20,7 +20,7 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username", nullable = false, unique = true)
+    @Column(name = "username", nullable = false)
     private String username;
 
     @Column(name = "provider", nullable = false)
